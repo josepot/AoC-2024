69ec8bc61e65fd56d870902252a690e3ad8e8c71
Josep M Sobrepere <jm.sobrepere@gmail.com>
2024-12-06 16:28:37 +0100

    cleanup a bit

--- src/6/solution.ts
+++ src/6/solution.ts
@@ -1,55 +1,15 @@
 import {
-  Direction,
   getDirectionWheel,
   getPositionFromKey,
   movePosition,
-  turnWheel,
   type Position,
 } from "../utils/directions"
 import { readGrid } from "../utils/readGrid"
 
-interface Point {
-  x: number
-  y: number
-}
-
-const solution1 = (lines: string[]) => {
-  let guardPos: Position = { x: 0, y: 0, key: "0-0" }
+const getSharedInfo = (lines: string[]) => {
+  let initialGuardPos: Position = { x: 0, y: 0, key: "0,0" }
   const grid = readGrid(lines, (x, pos) => {
-    if (x === "^") guardPos = { ...pos, key: `${pos.x}-${pos.y}` }
-    return x === "#"
-  })
-
-  const guard = {
-    direction: getDirectionWheel(),
-    position: guardPos,
-  }
-
-  const visited = new Set<string>()
-  while (
-    guard.position.x > 0 &&
-    guard.position.y > 0 &&
-    guard.position.x < grid.N_COLS &&
-    guard.position.y < grid.N_ROWS
-  ) {
-    visited.add(guard.position.key)
-
-    const nextPosition = movePosition(guard.position, guard.direction.value)
-    if (grid.getCell(nextPosition.x, nextPosition.y)) {
-      guard.direction = guard.direction.right
-    } else {
-      guard.position = nextPosition
-    }
-  }
-  visited.delete(guard.position.key)
-
-  return visited.size
-}
-
-const solution2 = (lines: string[]) => {
-  let initialGuardPos: Position = { x: 0, y: 0, key: "0-0" }
-  const grid = readGrid(lines, (x, pos) => {
-    if (x === "^") initialGuardPos = { ...pos, key: `${pos.x}-${pos.y}` }
+    if (x === "^") initialGuardPos = { ...pos, key: `${pos.x},${pos.y}` }
     return x === "#"
   })
 
@@ -60,30 +20,36 @@ const solution2 = (lines: string[]) => {
     }
 
     const visited = new Set<string>()
-    while (
+    do {
+      visited.add(guard.position.key)
+      const nextPosition = movePosition(guard.position, guard.direction.value)
+      if (grid.getCell(nextPosition.x, nextPosition.y))
+        guard.direction = guard.direction.right
+      else guard.position = nextPosition
+    } while (
       guard.position.x > 0 &&
       guard.position.y > 0 &&
       guard.position.x < grid.N_COLS &&
       guard.position.y < grid.N_ROWS
-    ) {
-      visited.add(guard.position.key)
-
-      const nextPosition = movePosition(guard.position, guard.direction.value)
-      if (grid.getCell(nextPosition.x, nextPosition.y)) {
-        guard.direction = guard.direction.right
-      } else {
-        guard.position = nextPosition
-      }
-    }
-    visited.delete(guard.position.key)
+    )
     return visited
   }
 
-  const isLoop = (position: Position) => {
-    const guard = {
+  return {
+    candidates: getCandidates(),
+    grid,
+    initialGuard: {
       direction: getDirectionWheel(),
       position: initialGuardPos,
-    }
+    },
+  }
+}
+
+const solution1 = (lines: string[]) => getSharedInfo(lines).candidates.size
+const solution2 = (lines: string[]) => {
+  const { grid, candidates, initialGuard } = getSharedInfo(lines)
+  const isLoop = (position: Position) => {
+    const guard = { ...initialGuard }
 
     const visited = new Set<string>()
     do {
@@ -108,8 +74,7 @@ const solution2 = (lines: string[]) => {
         return false
     } while (true)
   }
-
-  return [...getCandidates()].map(getPositionFromKey).filter(isLoop).length
+  return [...candidates].map(getPositionFromKey).filter(isLoop).length
 }
 
 export default [solution1, solution2]
