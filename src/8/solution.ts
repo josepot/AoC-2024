0a58dc3f500e7dec521c677227bf582cbde25c26
Josep M Sobrepere <jm.sobrepere@gmail.com>
2024-12-08 06:41:36 +0100

    Day 8

---
+++ src/8/solution.ts
@@ -0,0 +1,61 @@
+import { readGrid } from "../utils/readGrid"
+import { type Position } from "../utils/directions"
+
+const getSharedInfo = (lines: string[]) => {
+  const antenas = new Map<string, Position[]>()
+  const grid = readGrid(lines, (x, pos) => {
+    if (x !== ".") {
+      const positions = antenas.get(x) ?? []
+      positions.push({ ...pos, key: `${pos.x},${pos.y}` })
+      antenas.set(x, positions)
+    }
+    return x === "#"
+  })
+
+  return { grid, antenas }
+}
+
+const solution =
+  (isSecond = false) =>
+  (lines: string[]) => {
+    const { antenas, grid } = getSharedInfo(lines)
+
+    const frequencies = new Set<string>()
+    antenas.forEach((positions) => {
+      if (positions.length < 2) return
+
+      for (let i = 0; i < positions.length - 1; i++) {
+        for (let z = i + 1; z < positions.length; z++) {
+          if (isSecond) {
+            frequencies.add(positions[i].key)
+            frequencies.add(positions[z].key)
+          }
+
+          let xDelta = positions[i].x - positions[z].x
+          let yDelta = positions[i].y - positions[z].y
+          let x = positions[i].x + xDelta
+          let y = positions[i].y + yDelta
+          while (x >= 0 && x < grid.N_COLS && y >= 0 && y < grid.N_ROWS) {
+            frequencies.add(`${x},${y}`)
+            if (!isSecond) break
+            x += xDelta
+            y += yDelta
+          }
+
+          xDelta = positions[z].x - positions[i].x
+          yDelta = positions[z].y - positions[i].y
+          x = positions[z].x + xDelta
+          y = positions[z].y + yDelta
+          while (x >= 0 && x < grid.N_COLS && y >= 0 && y < grid.N_ROWS) {
+            frequencies.add(`${x},${y}`)
+            if (!isSecond) break
+            x += xDelta
+            y += yDelta
+          }
+        }
+      }
+    })
+    return frequencies.size
+  }
+
+export default [solution(), solution(true)]
