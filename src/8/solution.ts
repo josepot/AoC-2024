616391405c551f0d668ea6cf40ef4c64a01fd4d8
Josep M Sobrepere <jm.sobrepere@gmail.com>
2024-12-08 06:58:51 +0100

    cleanup a bit

--- src/8/solution.ts
+++ src/8/solution.ts
@@ -23,16 +23,16 @@ const solution =
     const frequencies = new Set<string>()
     antenas.forEach((positions) => {
       if (positions.length < 2) return
+      if (isSecond)
+        positions.forEach(({ key }) => {
+          frequencies.add(key)
+        })
 
       for (let i = 0; i < positions.length - 1; i++) {
         for (let z = i + 1; z < positions.length; z++) {
-          if (isSecond) {
-            frequencies.add(positions[i].key)
-            frequencies.add(positions[z].key)
-          }
-
           let xDelta = positions[i].x - positions[z].x
           let yDelta = positions[i].y - positions[z].y
+
           let x = positions[i].x + xDelta
           let y = positions[i].y + yDelta
           while (x >= 0 && x < grid.N_COLS && y >= 0 && y < grid.N_ROWS) {
@@ -42,15 +42,13 @@ const solution =
             y += yDelta
           }
 
-          xDelta = positions[z].x - positions[i].x
-          yDelta = positions[z].y - positions[i].y
-          x = positions[z].x + xDelta
-          y = positions[z].y + yDelta
+          x = positions[z].x - xDelta
+          y = positions[z].y - yDelta
           while (x >= 0 && x < grid.N_COLS && y >= 0 && y < grid.N_ROWS) {
             frequencies.add(`${x},${y}`)
             if (!isSecond) break
-            x += xDelta
-            y += yDelta
+            x -= xDelta
+            y -= yDelta
           }
         }
       }
